85523cacaaac51ef667dc907e919c5579b56c995
André Costa da Silva <andre91.net@gmail.com>
2018-06-02 13:29:41 -0300

    aula 6 - introducao templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ex JS', 'Angular'];
+
+  constructor() {
+    this.nomePortal = 'http: loane.training';
+  }
 
   ngOnInit() {
   }
