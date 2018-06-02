183fe8c5a4e4eb2624e326f6b7ae96d78dc8c8e8
André Costa da Silva <andre91.net@gmail.com>
2018-06-02 13:04:10 -0300

    commit inicial projeto angular 2+ loiane

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CursosModule } from './cursos/cursos.module';
 
 import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuComponenteComponent,
+    MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
